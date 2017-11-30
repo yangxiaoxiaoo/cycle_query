49304774dc43b4af0f3c8eaf905b872482f1b1d3
Xiaofeng <yangxiaoxiaoo@163.com>
2017-11-30 00:21:55 -0500

    added cycle insertion and priority value

--- cycle_query/globalclass.py
+++ cycle_query/globalclass.py
@@ -2,13 +2,71 @@ import functools
 
 @functools.total_ordering
 class PEI():
-    #class of partially explored instance
+    #class of partially explored instance, instance can be any shape later, cycle_instance for now
     #maintaining a field of starting variable for join consistency check later
     def __init__(self):
-        self.instance = None
-        self.wgt = 0
+        self.instance = cycle_instance()
+        self.wgt = 0.0
+        self.hrtc = 0.0
+        self.breakpoint = 0
     def __gt__(self, other):
-        return self.wgt > other.wgt
+        return (self.wgt + self.hrtc) > (other.wgt + other.hrtc)
     def __eq__(self, other):
-        return self.wgt == other.wgt
-    def merge(self, relation): #merge all partially explored trees
+        return (self.wgt + self.hrtc) == (other.wgt + other.hrtc)
+    def merge(self, new_tuple, tuple2weight, rel2tuple):
+        #merge this partially explored tree with newly added relation
+        if self.instance.length == 0:
+            #first insertion, init breakpoint for later check of consistency
+            self.breakpoint = new_tuple[0]
+        self.instance.insert_relation(new_tuple)
+        self.wgt += tuple2weight[new_tuple]
+        self.hrtc = self.instance.max_wgt_rem(tuple2weight, self.breakpoint)
+
+
+
+class cycle_instance():
+    #will need to extend later to support generic tree case,
+    #should make sure that they are consistent
+    def __init__(self):
+        #initialize the tuples to dummy values
+        self.R0 = (0, 0)
+        self.R1 = (0, 0)
+        self.R2 = (0, 0)
+        self.R3 = (0, 0)
+        self.length = 0
+        self.completion = False
+
+
+    def insert_relation(self, newtuple):
+        #take tuple, insert into instance
+        assert self.length in {1, 2, 3, 4}
+        assert self.completion == False
+        if self.length == 0:
+            self.R0 = newtuple
+            self.length = 1
+        elif self.length == 1:
+            assert self.R0[1] == newtuple[0]
+            self.R1 = newtuple
+            self.length = 2
+        elif self.length == 2:
+            assert self.R1[1] == newtuple[0]
+            self.R2 = newtuple
+            self.length = 3
+        elif self.length == 3:
+            assert self.R2[1] == newtuple[0] and self.R0[1] == newtuple[1]
+            self.R3 = newtuple
+            self.length = 4
+            self.completion = True
+
+
+
+    def max_wgt_rem(self, tuple2rem, a):
+        #return the max wgt for the remaining part -- heaviest weight case, its the upper bound
+        if self.length == 4:
+            return 0.0
+        if self.length == 3:
+            return tuple2rem[(self.R2, a)]
+        if self.length == 2:
+            return tuple2rem[(self.R1, a)]
+        if self.length == 1:
+            return tuple2rem[(self.R0, a)]
--- cycle_query/semi_join_utils.py
+++ cycle_query/semi_join_utils.py
@@ -44,15 +44,49 @@ def build_relation(cyc_len, var2cand, weightrange):
     return rel2tuple, tuple2weight
 
 def semi_join(R_start, R_end, rel2tuple):
+    tu2down_neis = dict()
+    tu2up_neis = dict()
     print "reduce " + R_start + " according to "+R_end + '...'
     #R_start and R_end are both keys in rel2tuple
     get_first_ele = lambda (x0, x1): x0
     checkset = set((get_first_ele(tup) for tup in rel2tuple[R_end]))
     new_set = set()
     for tup in rel2tuple[R_start]:
+        tu2down_neis[tup] = set()
         if tup[1] in checkset:
             new_set.add(tup)
+            for tup_down in rel2tuple[R_end]:
+                if tup_down[0] == tup[1]:
+                #[DESIGN CHOICE]when there are many spurious tuples, this is more efficient than loop earlier
+                    tu2down_neis[tup].add(tup_down)
+                    if tup_down in tu2up_neis:
+                        tu2up_neis[tup_down].add(tup)
+                    else:
+                        tu2up_neis[tup_down] = {tup}
     rel2tuple[R_start] = new_set
+    return tu2down_neis, tu2up_neis
+
+def full_SJ_reduce_4(rel2tuple):
+    #contain hard code for 4-cycle and will rewrite later...
+    #semi-join reduction for all relations, and return an index from tuple to downstream and upstream neighbors
+    #No global consistency: R0 to upstream and R0 to downstream
+    tu2down_neis, tu2up_neis = semi_join('R0', 'R1', rel2tuple)
+    tu2down_neis1, tu2up_neis1 = semi_join('R1', 'R2', rel2tuple)
+    tu2up_neis.update(tu2up_neis1)
+    tu2down_neis.update(tu2down_neis1)
+    tu2down_neis1, tu2up_neis1 = semi_join('R2', 'R3', rel2tuple)
+    tu2up_neis.update(tu2up_neis1)
+    tu2down_neis.update(tu2down_neis1)
+    tu2down_neis1, tu2up_neis1 = semi_join('R3', 'R0', rel2tuple)
+    print tu2down_neis1
+    tu2up_neis.update(tu2up_neis1)
+    tu2down_neis.update(tu2down_neis1)
+
+    print tu2down_neis
+    return tu2down_neis, tu2up_neis
+    #[DESIGN CHOICE]Q: shall we omit this and record a hash to max item only?
+    #[DESIGN CHOICE]A: No since we will need the hash to neighbors anyway later to expend PEI
+
 
 def heavy_map(rel2tuple):
     #take a tuple candidate map, return a map to boolean values indicating each relation is heavy or not
@@ -67,7 +101,50 @@ def heavy_map(rel2tuple):
             result[k] = True
     return result
 
-#def priority_search():
+def huristic_build_4(tuple2weight, rel2tuple, tu2down_neis):
+    #build a dictionary from (tuple, breakpoint) down to the remaining weight not including tuple
+    #assumption: no tuple appear in different relations
+    #later should change it into a while loop for any n...
+    tuple2rem = dict()
+    breakpoints = set()
+    for tu in rel2tuple['R3']:
+        breakpoints.add(tu[1]) #a set of all break variables for later loop over
+
+    #a = R3[1], compared later when starting from R0[0]
+    for tu in rel2tuple['R2']:
+        for tu_down in tu2down_neis[tu]:
+            new_val = tuple2weight[tu_down]
+            if (tu, tu_down[1]) in tuple2rem:
+                tuple2rem[(tu, tu_down[1])] = max(tuple2rem[(tu, tu_down[1])], new_val)
+            else:
+                tuple2rem[(tu, tu_down[1])] = new_val
+    for tu in rel2tuple['R1']:
+        for tu_down in tu2down_neis[tu]:
+            new_val = tuple2weight[tu_down]
+            for bp in breakpoints:
+                if (tu, bp) in tuple2rem:
+                    tuple2rem[(tu, bp)] = max(tuple2rem[(tu, bp)], tuple2rem[(tu_down, bp)] + new_val)
+                    #the same breakpoint
+                else:
+                    tuple2rem[(tu, bp)] = tuple2rem[(tu_down, bp)] + new_val
+    for tu in rel2tuple['R0']:
+        for tu_down in tu2down_neis[tu]:
+            new_val = tuple2weight[tu_down]
+            for bp in breakpoints:
+                if (tu, bp) in tuple2rem:
+                    tuple2rem[(tu, bp)] = max(tuple2rem[(tu, bp)], tuple2rem[(tu_down, bp)] + new_val)
+                    # the same breakpoint
+                else:
+                    tuple2rem[(tu, bp)] = tuple2rem[(tu_down, bp)] + new_val
+    print "(tuple, breakpoint) to remaining heuristic value mapping:"
+    print tuple2rem
+    return tuple2rem
+
+
+
+#TODO: def priority_search(k):
+    #push PEIs into a priority queue, pop k heaviest full items
+
 
 
 
@@ -77,7 +154,10 @@ var2cand = build_data(4, degrees)
 min_relations, tuple2weight = build_relation(4, var2cand, weightrange=10)
 min_relations['R1'].add((1, 10)) #adding a spurious tuple
 print "size before semi join reduction: "+str(len(min_relations['R1']))
-semi_join('R1', 'R2', min_relations)
+tu2down_neis0, tu2up_neis0 = semi_join('R1', 'R2', min_relations)
 print "size after semi join reduction: "+str(len(min_relations['R1']))
 print heavy_map(min_relations)
 print "test message again"
+
+tu2down_neis, tu2up_neis = full_SJ_reduce_4(min_relations)
+tuple2rem = huristic_build_4(tuple2weight, min_relations, tu2down_neis)
