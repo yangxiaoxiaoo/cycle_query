82c4ba9ba03f2ec575abe3643a0b6e8542c0404c
yangxiaofeng <yangxiaoxiaoo@163.com>
2017-12-01 02:06:33 -0500

    enumerate all implemented time measurement added

--- cycle_query/globalclass.py
+++ cycle_query/globalclass.py
@@ -2,30 +2,32 @@ import functools
 
 @functools.total_ordering
 class PEI():
-    #class of partially explored instance, instance can be any shape later, cycle_instance for now
-    #maintaining a field of starting variable for join consistency check later
+    # class of partially explored instance, instance can be any shape later, cycle_instance for now
+    # maintaining a field of starting variable for join consistency check later
     def __init__(self, tu, wgt, hrtc):
         self.instance = cycle_instance(tu)
         self.wgt = wgt
         self.hrtc = hrtc
         self.breakpoint = tu[0]
+
     def __gt__(self, other):
         return (self.wgt + self.hrtc) > (other.wgt + other.hrtc)
+
     def __eq__(self, other):
         return (self.wgt + self.hrtc) == (other.wgt + other.hrtc)
+
     def merge(self, new_tuple, tuple2weight, rel2tuple, tuple2rem):
-        #merge this partially explored tree with newly added relation
+        # merge this partially explored tree with newly added relation
         self.instance.insert_relation(new_tuple)
         self.wgt += tuple2weight[new_tuple]
         self.hrtc = self.instance.max_wgt_rem(tuple2rem, self.breakpoint)
 
 
-
 class cycle_instance():
-    #will need to extend later to support generic tree case,
-    #should make sure that they are consistent
+    # will need to extend later to support generic tree case,
+    # should make sure that they are consistent
     def __init__(self, tu):
-        #initialize the tuples to dummy values
+        # initialize the tuples to dummy values
         self.R0 = tu
         self.R1 = (0, 0)
         self.R2 = (0, 0)
@@ -42,10 +44,8 @@ class cycle_instance():
         elif self.length == 3:
             return self.R2
 
-
-
     def insert_relation(self, newtuple):
-        #take tuple, insert into instance
+        # take tuple, insert into instance
         assert self.length in {1, 2, 3, 4}
         assert self.completion == False
         if self.length == 0:
@@ -66,10 +66,8 @@ class cycle_instance():
             self.length = 4
             self.completion = True
 
-
-
     def max_wgt_rem(self, tuple2rem, a):
-        #return the min wgt for the remaining part -- lightest weight, lower bound H value
+        # return the min wgt for the remaining part -- lightest weight, lower bound H value
         if self.length == 4:
             return 0.0
         if self.length == 3:
--- cycle_query/semi_join_utils.py
+++ cycle_query/semi_join_utils.py
@@ -1,16 +1,18 @@
-#demo implementation for cyclic queries
-#utilities functions
+# demo implementation for cyclic queries
+# utilities functions
 import math
 import random
 import heapq
 import globalclass
 import copy
+import time
+
 
 def build_data(cyc_len, d):
-    #a hard coded function to create t-cycles, each relations is a tuple of two variables
-    #each has a cardinality of d[i]
+    # a hard coded function to create t-cycles, each relations is a tuple of two variables
+    # each has a cardinality of d[i]
 
-    var2cand = dict() #mapping from variable to its candidates
+    var2cand = dict() # mapping from variable to its candidates
     max_node = 0
     for variable in range(cyc_len):
         var2cand[variable] = set()
@@ -19,10 +21,11 @@ def build_data(cyc_len, d):
         max_node = max_node + d[variable]
     return var2cand
 
+
 def build_relation(cyc_len, var2cand, weightrange):
-    #build non-redundent relations based on the candidates, and randomly assign a weight to them
-    #weight is uniformly distributed in (0, weightrange)--can do other distributions later too.
-    #there can be multiple ways. I'm doing all to all dense connection now.
+    # build non-redundant relations based on the candidates, and randomly assign a weight to them
+    # weight is uniformly distributed in (0, weight_range)--can do other distributions later too.
+    # there can be multiple ways. I'm doing all to all dense connection now.
     rel2tuple = dict()
     tuple2weight = dict()
     for variable in range(cyc_len - 1):
@@ -30,7 +33,7 @@ def build_relation(cyc_len, var2cand, weightrange):
         rel2tuple[key] = set()
         for tuple0 in var2cand[variable]:
             for tuple1 in var2cand[variable + 1]:
-                #all pair add. Can do other stratergies...
+                # all pair add. Can do other strategies...
                 rel2tuple[key].add((tuple0, tuple1))
                 tuple2weight[(tuple0, tuple1)] = random.uniform(0, weightrange)
                 print key + ": adding tuple" + str([tuple0, tuple1])
@@ -38,17 +41,18 @@ def build_relation(cyc_len, var2cand, weightrange):
         rel2tuple[key] = set()
         for tuple0 in var2cand[cyc_len - 1]:
             for tuple1 in var2cand[0]:
-                # all pair add. Can do other stratergies...
+                # all pair add. Can do other strategies...
                 rel2tuple[key].add((tuple0, tuple1))
                 tuple2weight[(tuple0, tuple1)] = random.uniform(0, weightrange)
                 print key + ": adding tuple" + str([tuple0, tuple1])
     return rel2tuple, tuple2weight
 
+
 def semi_join(R_start, R_end, rel2tuple):
     tu2down_neis = dict()
     tu2up_neis = dict()
     print "reduce " + R_start + " according to "+R_end + '...'
-    #R_start and R_end are both keys in rel2tuple
+    # R_start and R_end are both keys in rel2tuple
     get_first_ele = lambda (x0, x1): x0
     checkset = set((get_first_ele(tup) for tup in rel2tuple[R_end]))
     new_set = set()
@@ -58,7 +62,7 @@ def semi_join(R_start, R_end, rel2tuple):
             new_set.add(tup)
             for tup_down in rel2tuple[R_end]:
                 if tup_down[0] == tup[1]:
-                #[DESIGN CHOICE]when there are many spurious tuples, this is more efficient than loop earlier
+                    # [DESIGN CHOICE]when there are many spurious tuples, this is more efficient than loop earlier
                     tu2down_neis[tup].add(tup_down)
                     if tup_down in tu2up_neis:
                         tu2up_neis[tup_down].add(tup)
@@ -67,10 +71,11 @@ def semi_join(R_start, R_end, rel2tuple):
     rel2tuple[R_start] = new_set
     return tu2down_neis, tu2up_neis
 
+
 def full_SJ_reduce_4(rel2tuple):
-    #contain hard code for 4-cycle and will rewrite later...
-    #semi-join reduction for all relations, and return an index from tuple to downstream and upstream neighbors
-    #No global consistency: R0 to upstream and R0 to downstream
+    # contain hard code for 4-cycle and will rewrite later...
+    # semi-join reduction for all relations, and return an index from tuple to downstream and upstream neighbors
+    # No global consistency: R0 to upstream and R0 to downstream
     tu2down_neis, tu2up_neis = semi_join('R0', 'R1', rel2tuple)
     tu2down_neis1, tu2up_neis1 = semi_join('R1', 'R2', rel2tuple)
     tu2up_neis.update(tu2up_neis1)
@@ -85,12 +90,12 @@ def full_SJ_reduce_4(rel2tuple):
 
     print tu2down_neis
     return tu2down_neis, tu2up_neis
-    #[DESIGN CHOICE]Q: shall we omit this and record a hash to max item only?
-    #[DESIGN CHOICE]A: No since we will need the hash to neighbors anyway later to expend PEI
+    # [DESIGN CHOICE]Q: shall we omit this and record a hash to max item only?
+    # [DESIGN CHOICE]A: No since we will need the hash to neighbors anyway later to expend PEI
 
 
 def heavy_map(rel2tuple):
-    #take a tuple candidate map, return a map to boolean values indicating each relation is heavy or not
+    # take a tuple candidate map, return a map to boolean values indicating each relation is heavy or not
     n = 0
     result = dict()
     for k, v in rel2tuple.iteritems():
@@ -102,16 +107,17 @@ def heavy_map(rel2tuple):
             result[k] = True
     return result
 
-def huristic_build_4(tuple2weight, rel2tuple, tu2down_neis):
-    #build a dictionary from (tuple, breakpoint) down to the remaining weight not including tuple
-    #assumption: no tuple appear in different relations
-    #later should change it into a while loop for any n...
+
+def heuristic_build_4(tuple2weight, rel2tuple, tu2down_neis):
+    # build a dictionary from (tuple, breakpoint) down to the remaining weight not including tuple
+    # assumption: no tuple appear in different relations
+    # later should change it into a while loop for any n...
     tuple2rem = dict()
     breakpoints = set()
     for tu in rel2tuple['R3']:
-        breakpoints.add(tu[1]) #a set of all break variables for later loop over
+        breakpoints.add(tu[1]) # a set of all break variables for later loop over
 
-    #a = R3[1], compared later when starting from R0[0]
+    # a = R3[1], compared later when starting from R0[0]
     for tu in rel2tuple['R2']:
         for tu_down in tu2down_neis[tu]:
             new_val = tuple2weight[tu_down]
@@ -125,7 +131,7 @@ def huristic_build_4(tuple2weight, rel2tuple, tu2down_neis):
             for bp in breakpoints:
                 if (tu, bp) in tuple2rem:
                     tuple2rem[(tu, bp)] = min(tuple2rem[(tu, bp)], tuple2rem[(tu_down, bp)] + new_val)
-                    #the same breakpoint
+                    # the same breakpoint
                 else:
                     tuple2rem[(tu, bp)] = tuple2rem[(tu_down, bp)] + new_val
     for tu in rel2tuple['R0']:
@@ -142,76 +148,98 @@ def huristic_build_4(tuple2weight, rel2tuple, tu2down_neis):
     return tuple2rem
 
 
+def fake_heuristic_4(tuple2weight, rel2tuple):
+    tuple2rem = dict()
+    breakpoints = set()
+    for tu in rel2tuple['R3']:
+        breakpoints.add(tu[1])
+
+    for tu in tuple2weight:
+        for bp in breakpoints:
+            tuple2rem[(tu, bp)] = 0
+    return tuple2rem
+
 
 def priority_search_4(K, rel2tuple, tuple2weight, tu2down_neis):
-    #push PEIs into a priority queue, pop k heaviest full items
-    #[DESIGN CHOICE] pop the lightest element as consistent with heapq native
+    # push PEIs into a priority queue, pop k heaviest full items
+    # [DESIGN CHOICE] pop the lightest element as consistent with heapq native
     TOP_K = []
     PQ = []
-    tuple2rem = huristic_build_4(tuple2weight, rel2tuple, tu2down_neis)
-    #for fair time measurement sake, tuple2rem should be part of prioritized search.
+    tuple2rem = heuristic_build_4(tuple2weight, rel2tuple, tu2down_neis)
+    # for fair time measurement sake, tuple2rem should be part of prioritized search.
     for tu in rel2tuple['R0']:
         heapq.heappush(PQ, globalclass.PEI(tu, tuple2weight[tu], tuple2rem[(tu, tu[0])]))
         print "PQpush"
     while len(PQ) != 0:
-        #defult: without termination, enumerate till PQ is empty
+        # defult: without termination, enumerate till PQ is empty
         cur_PEI = heapq.heappop(PQ)
         if cur_PEI.instance.completion:
             TOP_K.append(cur_PEI)
             if len(TOP_K) == K:
                 break
-        elif cur_PEI.instance.length != 3: #not completed, there is frontier, no need to check breakpoint
+        elif cur_PEI.instance.length != 3: # not completed, there is frontier, no need to check breakpoint
             frontier = cur_PEI.instance.frontier()
             for neighbor in tu2down_neis[frontier]:
                 new_PEI = copy.deepcopy(cur_PEI)
                 new_PEI.merge(neighbor, tuple2weight, rel2tuple, tuple2rem)
                 heapq.heappush(PQ, new_PEI)
-                #[DESIGN CHOICE] shall we keep the top 1 unpushed (outside always)?
-                #slightly more wordy to implement so I will defer this dicision later
-        else: #length == 3, check breakpoint
+                # [DESIGN CHOICE] shall we keep the top 1 unpushed (outside always)?
+                # slightly more wordy to implement so I will defer this dicision later
+        else:  # length == 3, check breakpoint
             frontier = cur_PEI.instance.frontier()
             for neighbor in tu2down_neis[frontier]:
                 if neighbor[1] == cur_PEI.breakpoint:
                     new_PEI = copy.deepcopy(cur_PEI)
                     new_PEI.merge(neighbor, tuple2weight, rel2tuple, tuple2rem)
                     heapq.heappush(PQ, new_PEI)
-                    #[DESIGN CHOICE] for more readable logic, no additional break here (extra push in heapQ)
+                    # [DESIGN CHOICE] for more readable logic, no additional break here (extra push in heapQ)
 
     print "TOP K results are"
     for PEI in TOP_K:
         print PEI.wgt
+    assert len(TOP_K) == K or len(PQ) == 0
     return TOP_K
 
+
 def enumerate_all_4(K, rel2tuple, tuple2weight, tu2down_neis):
-    #the enumeration baseline to compare with, implemented by regular join
+    # the enumeration baseline to compare with, implemented by regular join.
+    # also return a total count
     TOP_K = []
     Interm_results = []
-    fake_tuple2rem = ...#TODO: map all tuple to 0, only record starting point
+    fake_tuple2rem = fake_heuristic_4(tuple2weight, rel2tuple)
     for tu in rel2tuple['R0']:
         Interm_results.append(globalclass.PEI(tu, tuple2weight[tu], 0))
-        #all instances have 0 heuristics, therefore if there is a comparison is by wgt only.
-        #ALSO, Comp is not needed.
+        # all instances have 0 heuristics, therefore if there is a comparison is by wgt only.
+        # ALSO, Comp is not needed.
     while len(Interm_results) != 0:
-        for Inter_res in Interm_results:
-            frontier = Inter_res.instance.frontier()
-            for neighbor in tu2down_neis[frontier]:
+        Inter_res = Interm_results.pop()
+        frontier = Inter_res.instance.frontier()
+        for neighbor in tu2down_neis[frontier]:
+            if (Inter_res.instance.length == 3 and neighbor[1] == Inter_res.breakpoint) \
+                    or Inter_res.instance.length < 3:
                 new_PEI = copy.deepcopy(Inter_res)
                 new_PEI.merge(neighbor, tuple2weight, rel2tuple, fake_tuple2rem)
                 if new_PEI.instance.completion:
                     TOP_K.append(new_PEI)
+                    if len(TOP_K) == K:
+                        break
                 else:
                     Interm_results.append(new_PEI)
+
     TOP_K.sort()
-    return TOP_K
+    print "TOP K results are"
+    for PEI in TOP_K:
+        print PEI.wgt
+    assert len(TOP_K) == K or len(Interm_results) == 0
+    return TOP_K[:K], len(TOP_K)
 
 
 def test_priority_search():
-    #For now, without a heavy/light method, we just always assume R0 is heavy, without loss of generality.
+    # For now, without a heavy/light method, we just always assume R0 is heavy, without loss of generality.
     degrees = [2, 1, 3, 1]
-    n = sum(degrees)
     var2cand = build_data(4, degrees)
     min_relations, tuple2weight = build_relation(4, var2cand, weightrange=10)
-    min_relations['R1'].add((1, 10)) #adding a spurious tuple
+    min_relations['R1'].add((1, 10)) # adding a spurious tuple
     print "size before semi join reduction: "+str(len(min_relations['R1']))
     tu2down_neis0, tu2up_neis0 = semi_join('R1', 'R2', min_relations)
     print "size after semi join reduction: "+str(len(min_relations['R1']))
@@ -219,12 +247,36 @@ def test_priority_search():
     print "test message again"
 
     tu2down_neis, tu2up_neis = full_SJ_reduce_4(min_relations)
-    tuple2rem = huristic_build_4(tuple2weight, min_relations, tu2down_neis)
-    priority_search_4(100, min_relations, tuple2weight, tu2down_neis)
+    tuple2rem = heuristic_build_4(tuple2weight, min_relations, tu2down_neis)
+    TOP_K_PQ = priority_search_4(100, min_relations, tuple2weight, tu2down_neis)
+    TOP_K_enu, total = enumerate_all_4(100, min_relations, tuple2weight, tu2down_neis)
+    assert TOP_K_enu == TOP_K_PQ
 
-def time_measurements():
-    print "comparing running time of prioritied search and emumerate all"
+
+def time_measurements(degrees, K):
+    # return a pair of running time of prioritized search and enumerate all
+    var2cand = build_data(4, degrees)
+    min_relations, tuple2weight = build_relation(4, var2cand, weightrange=10)
+    tu2down_neis, tu2up_neis = full_SJ_reduce_4(min_relations)
+
+    start = time.time()
+    TOP_K_PQ = priority_search_4(K, min_relations, tuple2weight, tu2down_neis)
+    end = time.time()
+    TOP_K_enu, total = enumerate_all_4(K, min_relations, tuple2weight, tu2down_neis)
+    end2 = time.time()
+    time_PQ = end - start
+    time_enu = end2 - end
+
+    assert TOP_K_enu == TOP_K_PQ
+    print "===total count is==="
+    print total
+    print "=====running time=="
+    print time_PQ
+    print time_enu
+    return time_PQ, time_enu
 
 
 if __name__ == "__main__":
     test_priority_search()
+    time_measurements([20, 4, 7, 10], 100)
+  #  time_measurements([20, 4, 7, 10], 10000)
