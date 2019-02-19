b4edcb4854a862c484cf1002e32c7a538b233463
yangxiaoxiaoo <yangxiaoxiaoo@163.com>
2019-02-18 19:48:39 -0500

    real data parser on Yelp etc in process

--- cycle_query/data_loader.py
+++ cycle_query/data_loader.py
@@ -1,5 +1,15 @@
 import os
 
+def findcycle(neighbors, seed):
+    seen = {seed}
+    Q = [seed]
+    while Q:
+
+
+
+
+
+
 def load(dir, dataset):
     relation2tuple = dict()
     tuple2weight = dict()
@@ -10,12 +20,40 @@ def load(dir, dataset):
             lines = f.readlines()
         del lines[0]
 
+        relationTypes = set()
+        type2tuples = dict()
+
+
         for line in lines:
             node_fro = int(line.split()[0])
             node_to = int(line.split()[1])
             type_fro = int(line.split()[2].strip('[').strip(']'))
             type_to = int(line.split()[3].strip('[').strip(']'))
             wgt = float(line.split()[5])
+
+            type = (type_fro, type_to)
+            if type not in type2tuples:
+                relationTypes.add(type)
+                type2tuples[type] = {(node_fro, node_to)}
+            else:
+                type2tuples[type].add((node_fro, node_to))
+            tuple2weight[(node_fro, node_to)] = wgt
+
+        # DFS on set of relations to find path and cycle in schema.
+        neighbors = dict()
+
+        seed = 0
+        for type in relationTypes:
+            if type[0] not in neighbors:
+                neighbors[type[0]] = {type[1]}
+            else:
+                neighbors[type[0]].add(type[1])
+
+            seed = type[0]  # randomly..
+
+        cycle = findcycle(neighbors, seed)
+
+
 
 
 
